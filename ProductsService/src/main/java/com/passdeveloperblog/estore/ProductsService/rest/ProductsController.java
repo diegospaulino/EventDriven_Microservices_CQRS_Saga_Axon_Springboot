11ccca1dde469ad19669fe62e1260cb2845072be
Diego Paulino <diegopaulino023@gmail.com>
2025-07-15 15:50:23 -0300

    Inicialização do controller de produtos

---
+++ ProductsService/src/main/java/com/passdeveloperblog/estore/ProductsService/rest/ProductsController.java
@@ -0,0 +1,10 @@
+package com.passdeveloperblog.estore.ProductsService.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products") //http://localhost:8080/products
+public class ProductsController {
+
+}
