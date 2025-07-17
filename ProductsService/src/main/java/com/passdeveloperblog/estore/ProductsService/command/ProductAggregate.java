1a7c6a19a16556658da7ef4de9a756b83f8a185a
Diego Paulino <diegopaulino023@gmail.com>
2025-07-17 12:07:46 -0300

    Criação da classe de eventos para produtos criados e Atualização da classe productaggregate com os comandos de eventos para o product criado

--- ProductsService/src/main/java/com/passdeveloperblog/estore/ProductsService/command/ProductAggregate.java
+++ ProductsService/src/main/java/com/passdeveloperblog/estore/ProductsService/command/ProductAggregate.java
@@ -3,10 +3,23 @@ package com.passdeveloperblog.estore.ProductsService.command;
 import java.math.BigDecimal;
 
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+import com.passdeveloperblog.estore.ProductsService.core.events.ProductCreatedEvent;
 
 @Aggregate
 public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
 
     public ProductAggregate() {
 
@@ -20,5 +33,29 @@ public class ProductAggregate {
             throw new IllegalArgumentException("Preço não pode ser menor ou igual a zero!");
 
         }
+
+        if(createProductCommand.getTitle() == null || createProductCommand.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Título não pode ser nulo ou vazio!");
+        }
+
+        //Copia os dados do objeto createProductCommand para o objeto productCreatedEvent
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+        BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
+
+        //Dispara para os diferentes eventos desse aggregate um evento 
+        //informado que o estado desse aggregate pode ser atualizado com novas informações
+        AggregateLifecycle.apply(productCreatedEvent);
     }
+
+    @EventSourcingHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+        //Esse método é chamado quando o evento ProductCreatedEvent é disparado
+        //O Axon Framework irá automaticamente chamar esse método para atualizar o estado do aggregate
+        //com as informações do evento recebido
+
+        this.productId = productCreatedEvent.getProductId();
+        this.title = productCreatedEvent.getTitle();
+        this.price = productCreatedEvent.getPrice();
+        this.quantity = productCreatedEvent.getQuantity();
+    }   
 }
