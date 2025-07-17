3022800b1cb8ea54dcd94d6db637344158e543d8
Diego Paulino <diegopaulino023@gmail.com>
2025-07-16 21:34:17 -0300

    Ajuste do nome do package na branch original de desenvolvimento do spring cloud api aion server

--- ApiGateway/src/main/java/com/appsdeveloperblog/estore/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/appsdeveloperblog/estore/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.appsdeveloperblog.estore.ApiGateway;
+package com.appsdeveloperblog.estore.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
